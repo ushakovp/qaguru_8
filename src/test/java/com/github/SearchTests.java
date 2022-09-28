a5a113902c2ddbe4841ed3bdb639bbff40c10711
Pavel Ushakov <PUshakov@ita-labs.ru>
2022-09-28 09:51:12 +0400

    Second parametrized test

--- src/test/java/com/github/SearchTests.java
+++ src/test/java/com/github/SearchTests.java
@@ -1,9 +1,11 @@
 package com.github;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -21,4 +23,20 @@ public class SearchTests {
         $("[data-test-selector=nav-search-input]").submit();
         $(byText(testData)).should(exist);
     }
+
+    @CsvSource(value = {
+            "selenide, Concise UI Tests with Java!",
+            "selenoid, Selenium Hub successor running browsers within containers.",
+            "playwright, Playwright is a framework for Web Testing and Automation."
+    })
+    @ParameterizedTest(name = "На странице поиска, для  \"{0}\" есть описание -  \"{0}\"")
+    void tst(String testData, String expectedResult) {
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").click();
+        $("[data-test-selector=nav-search-input]").setValue(testData);
+        $("[data-test-selector=nav-search-input]").submit();
+        $(byText(testData)).should(exist);
+        $$("li.repo-list-item").first().shouldHave(text(expectedResult));
+    }
+
 }
